e0c1e4704f46625b7fa98a1ce7bdb8a059a174e6
sunny <dyoon0617@gmail.com>
2019-02-04 14:10:19 +0900

    Pass the [M1902].cpp

--- [M1902].cpp
+++ [M1902].cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 
 using namespace std;
 
@@ -13,17 +12,32 @@ bool check(bool alpa[]) {
 	return true;
 }
 
+int arr_size(int index) {
+	for (int cnt1 = 0; cnt1 < 101; cnt1++) {
+		if (arr[index][cnt1] == NULL) {
+			return cnt1;
+		}
+	}
+	return 101;
+}
+
 void arr_check(int index, bool alpa[]) {
-	for (int cnt1 = 0; cnt1 < arr[index].size(); cnt1++) {
-		alpa[arr[index].at(cnt1) - 'a'] = true;
+	for (int cnt1 = 0; cnt1 < arr_size(index); cnt1++) {
+		alpa[arr[index][cnt1] - 'a'] = true;
 	}
 }
 
+int gub(int index) {
+	int a = 1;
+	for (int cnt1 = 1; cnt1 <= n - index; cnt1++) {
+		a *= 2;
+	}
+	return a;
+}
 int dp(int index, bool alpa[]) {
-
+	if (check(alpa)) return gub(index);
 	if (index == n) return 0;
-	if (check(alpa)) return 1;
-
+	
 	bool alpa_copy[26];
 	for (int cnt1 = 0; cnt1 < 26; cnt1++) {
 		alpa_copy[cnt1] = alpa[cnt1];
@@ -38,21 +52,29 @@ int main(int argc, char** argv)
 	int test_case;
 	int T;
 
-	//freopen("input.txt", "r", stdin);
-
 	cin >> T;
 
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
+		for (int cnt1 = 0; cnt1 < 16; cnt1++) {
+			for (int cnt2 = 0; cnt2 < 101; cnt2++) {
+				arr[cnt1][cnt2] = NULL;
+			}
+		}
+
+		bool alpa[26];
+
+		for (int cnt1 = 0; cnt1 < 26; cnt1++) {
+			alpa[cnt1] = false;
+		}
+
 		cin >> n;
 
 		for (int cnt1 = 0; cnt1 < n; cnt1++) {
 			cin >> arr[cnt1];
 		}
 
-		bool alpa[26];
-
-		cout << dp(0, alpa);
+		cout << "#" << test_case << " " << dp(0, alpa) << "\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
